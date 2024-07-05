ff374ed225d035cf01c40b5b50f16924b8ed9b0a
yanquankun <yanquankun@58.com>
2024-07-05 21:06:11 +0800

    feat: 0.0.2版本

--- src/extension.js
+++ src/extension.js
@@ -2,12 +2,40 @@ const vscode = require("vscode");
 const zhoubao = require("./plugins/zhoubao");
 const zbtimer = zhoubao();
 let zbtimerId = null;
+let pluginInstalled = false;
+
+function refreshVSCode() {
+  // 重新加载VSCode
+  vscode.commands.executeCommand("workbench.action.reloadWindow");
+}
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
   console.log("58-helper插件打开");
+
+  // 检查插件是否已安装
+  if (!pluginInstalled) {
+    // 如果未安装，显示提示并记录已安装
+    const message = "检测到安装了58-helper插件，请点击刷新按钮重置vscode";
+    const commandId = "58-vscode-restart";
+    vscode.window
+      .showInformationMessage(message, "reload")
+      .then((selection) => {
+        pluginInstalled = true;
+        if (selection === "reload") {
+          setTimeout(() => {
+            refreshVSCode();
+          }, 3000);
+          context.subscriptions.push(
+            vscode.commands.registerCommand(commandId, () => {
+              refreshVSCode();
+            })
+          );
+        }
+      });
+  }
 
   // 周报注册
   zbtimerId = zbtimer();
